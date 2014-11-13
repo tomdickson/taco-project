9fb5c7acc93c65067540856bd38012ebe8c66eb4
Tom Dickson <thosdickson@gmail.com>
2014-11-13 15:17:06 -0800

    This incl hide/show controls with 'Get Trek'

--- tacoProj/js/taco3.js
+++ tacoProj/js/taco3.js
@@ -2,24 +2,33 @@ var map;
 var pos;
 var infowindow;
 var markers = [];
-function initialize() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 13,
-    center: new google.maps.LatLng(37.77,-122.4)
-  });
-};
+var latarray = [];
+var directionsDisplay;
+var directionsService;
+var map;
+
 function setLocation() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+  map = new google.maps.Map(document.getElementById('map-canvas'),{
+    scrollwheel: false,
+  });
+
+  directionsDisplay.setMap(map);
+
   // Try HTML5 geolocation
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
     
     pos = new google.maps.LatLng(position.coords.latitude, 
       position.coords.longitude);
+
     var foundme = new google.maps.InfoWindow({
       map: map,
       position: pos,
       content: 'You are here!'
     });
+
       map.setCenter(pos);
       map.setZoom(16);
     }, function() {
@@ -29,162 +38,82 @@ function setLocation() {
     // Browser doesn't support Geolocation
     handleNoGeolocation(false);
   }
-}
+};
+
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
     }
   }
-}
+};
+
 function createMarker(place) {
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
+
   markers.push(marker);
+  
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name);
     infowindow.open(map, this);
   });
-}
-// Sets the map on all markers in the array.
-function setAllMap(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
 };
+
 // Removes the markers from the map, but keeps them in the array.
 function clearMarkers() {
-  setAllMap(null);
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
 };
-function findFoodLatin(pos) {
+
+function find(type, keyword, radius, pos) {
+  clearMarkers();
   var request = {
     location: pos,
-    radius: 500,
-    types: ['food'],
-    keyword:['mexican']
+    radius: radius,
+    types: type,
+    keyword: keyword
   };
   infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
 };
-function findFoodAsian(pos) {
+
+function calcRoute(markers, pos) {
+  clearMarkers();
+
+  for (var i = 0; i < markers.length; i++) {
+    latarray.push({
+        location: new google.maps.LatLng(markers[i].position.k, markers[i].position.B),
+        stopover: true
+      });
+    }
+  var waypts = latarray.slice(0, latarray.length < 8 ? latarray.length : 8);
+
+  // var start = pos;
+  // var end = latarray[latarray.length-1];
+  var start = new google.maps.LatLng(pos.k, pos.B);
+  var end = new google.maps.LatLng(37.7749295,-122.4194);
   var request = {
-    location: pos,
-    radius: 500,
-    types: ['food'],
-    keyword:['chinese']
+      origin:start,
+      destination:end,
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.WALKING
   };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findFoodItal(pos) {
-  var request = {
-    location: pos,
-    radius: 500,
-    types: ['food'],
-    keyword:['italian']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findFooDeli(pos) {
-  var request = {
-    location: pos,
-    radius: 500,
-    types: ['food'],
-    keyword:['deli']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findFoodBbq(pos) {
-  var request = {
-    location: pos,
-    radius: 500,
-    types: ['food'],
-    keyword:['bbq']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findFoodVeggie(pos) {
-  var request = {
-    location: pos,
-    radius: 500,
-    types: ['food'],
-    keyword:['vegetarian']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findMovie(pos) {
-  var request = {
-    location: pos,
-    radius: 750,
-    types: ['movie_theater','theatre']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findStore(pos) {
-  var request = {
-    location: pos,
-    radius: 500,
-    types: ['bicycle_store','book_store','department_store','shopping_mall','jewelry_store','clothing_store']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findGallery(pos) {
-  var request = {
-    location: pos,
-    radius: 500,
-    types: ['art_gallery','museum']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findAmuse(pos) {
-  var request = {
-    location: pos,
-    radius: 1000,
-    types: ['bowling_alley','amusement_park','zoo','stadium']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findClub(pos) {
-  var request = {
-    location: pos,
-    radius: 500,
-    types: ['night_club']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
-function findPark(pos) {
-  var request = {
-    location: pos,
-    radius: 750,
-    types: ['park']
-  };
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-};
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      console.log("Routing did not work.");
+    }
+  });
+}
+
 function handleNoGeolocation(errorFlag) {
   if (errorFlag) {
     var content = 'Oh man, we lost you.';
@@ -200,68 +129,65 @@ function handleNoGeolocation(errorFlag) {
   var infowindow = new google.maps.InfoWindow(options);
   map.setCenter(options.position);
 };
-google.maps.event.addDomListener(window, 'load', initialize);
+
+google.maps.event.addDomListener(window, 'load', setLocation);
+
 $(document).ready(function () {
-  $("#locate").click(function () {
-    setLocation();
-  });
   $("#food").click(function(){
     $(".headerB").removeClass("hidden");
     $("#sliders").removeClass("hidden");
     $(".headerC").addClass("hidden");
   });
-   $("#fun").click(function(){
+  $("#fun").click(function(){
     $(".headerC").removeClass("hidden");
     $("#sliders").removeClass("hidden");
     $(".headerB").addClass("hidden");
   });
   $("#foodLatin").click(function () {
-    clearMarkers();
-    findFoodLatin(pos);
+    find(['food'], ['mexican'], 500, pos);
   });
   $("#foodAsian").click(function () {
-    clearMarkers();
-    findFoodAsian(pos);
+    find(['food'], ['chinese'], 500, pos);
   });
   $("#foodItal").click(function () {
-    clearMarkers();
-    findFoodItal(pos);
+    find(['food'], ['italian'], 500, pos);
   });
-  $("#fooDeli").click(function () {
-    clearMarkers();
-    findFooDeli(pos);
+  $("#foodDeli").click(function () {
+    find(['food'], ['deli'], 500, pos);
   });
   $("#foodBbq").click(function () {
-    clearMarkers();
-    findFoodBbq(pos);
+    find(['food'], ['bbq'], 500, pos);
   });
   $("#foodVeggie").click(function () {
-    clearMarkers();
-    findFoodVeggie(pos);
+    find(['food'], ['vegetarian'], 500, pos);
   });
   $("#theatre").click(function () {
-    clearMarkers();
-    findMovie(pos);
+    find(['movie_theater','theatre'], undefined, 500, pos);
   });
   $("#store").click(function () {
-    clearMarkers();
-    findStore(pos);
+    find(['bicycle_store','book_store','department_store','shopping_mall','jewelry_store','clothing_store'], undefined, 500, pos);
   });
   $("#gallery").click(function () {
-    clearMarkers();
-    findGallery(pos);
+    find(['art_gallery','museum'], undefined, 500, pos);
   });
   $("#amuse").click(function () {
-    clearMarkers();
-    findAmuse(pos);
+    find(['bowling_alley','amusement_park','zoo','stadium'], undefined, 500, pos);
   });
   $("#club").click(function () {
-    clearMarkers();
-    findClub(pos);
+    find(['night_club'], undefined, 500, pos);
   });
   $("#park").click(function () {
-    clearMarkers();
-    findPark(pos);
+    find(['park'], undefined, 500, pos);
   });
-  
+  $("#path").click(function () {
+    calcRoute(markers, pos);
+    $("#sliders").addClass("hidden");
+    $(".headerC").addClass("hidden");
+    $(".headerB").addClass("hidden");
+    $(".headerA").addClass("hidden");  
+  });
+  $("#goBack").click(function () {
+    $(".headerA").removeClass("hidden");  
+  });
+
 });
